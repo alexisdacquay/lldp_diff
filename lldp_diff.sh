b392604b89e6e1063f85bf60c7d40c98819f5266
Alexis Dacquay <adacquay@gmail.com>
2017-06-02 10:52:53 +0200

    Add verifications. Add comments. Tested on Arista EOS 4.18.1

--- lldp_diff.sh
+++ lldp_diff.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # by Alexis Dacquay, ad@arista.com
 # version 0.4
 #
@@ -7,23 +7,36 @@
 # schedule lldp_diff interval 60 max-log-files 10 command bash /mnt/flash/lldp_diff.sh
 # !
 
+if [ $1 ]
+    then FILES_TO_KEEP=$1
+    else FILES_TO_KEEP='10'
+fi
 LOC='/mnt/flash/schedule/lldp_diff'
+# MUST DO: change the below to a valid email address
+EMAIL_DEST='user@domain.com'
+
 # Create the directory if it does not exist
 mkdir -p $LOC
-cd $LOC
-# debug: echo `pwd`
-# Move the "latest" capture as "previous" (aka old). "2> /dev/null" to silent in case "latest" does not exist yet (e.g. at 1st run)
-mv lldp_diff-latest lldp_diff-previous 2> /dev/null
-# Capture of the LLDP table, skipping the first line since it would always be different (e.g. Last table change time   : 2:00:44 ago)
+
+# Move the "latest" capture as "previous" (aka old). "2> /dev/null" to silent
+# in case "latest" does not exist yet (e.g. at 1st run)
+mv $LOC/lldp_diff-latest $LOC/lldp_diff-previous 2> /dev/null
+
+# Capture of the LLDP table, skipping the first line since it would always be 
+# different (e.g. Last table change time   : 2:00:44 ago)
 FastCli -p 15 -c $'show lldp neighbors | tail -n +2' > $LOC/lldp_diff-latest
 
-# Optional: for historical storage, uncomment the two below lines (you must tidy yourself after a while)
-# NOW=$(date +%Y-%m-%d.%H%M%S)
-# cp lldp_diff-latest lldp_diff-$NOW
+# Historical records, uncompressed
+NOW=$(date +%Y-%m-%d.%H%M%S)
+cp $LOC/lldp_diff-latest $LOC/lldp_diff-$NOW
 
-DIFF=$(diff lldp_diff-latest lldp_diff-previous)
-if [ "$DIFF" != "" ] 
-then
-    echo "$DIFF" | email -i -s "[LLDP Diff on $HOSTNAME]" ad.arista.01@gmail.com
+# IF there are too many files, we delete the older ones
+if [ $(ls $LOC | wc -l) -gt $FILES_TO_KEEP ]
+    then ls -t | tail -n +$FILES_TO_KEEP | xargs -d '\n' rm
 fi
 
+# Sending an email if there is a difference. '-N' is case '-previous' is nil
+DIFF=$(diff -N $LOC/lldp_diff-latest $LOC/lldp_diff-previous)
+if [ "$DIFF" != "" ] 
+    then echo "$DIFF" | email -i -s "[LLDP Diff on $HOSTNAME]" $EMAIL_DEST
+fi
